c493ae8be8933e109a2b0815d93e631d83e874bf
daniel garcia vaglio <degv364@gmail.com>
2017-07-15 14:49:54 -0600

    Fixing coding style

--- scripts/tcp/client_tcp.py
+++ scripts/tcp/client_tcp.py
@@ -3,20 +3,20 @@ import sys
 
 #Este comando lo que genera es un modulo de socket de la libreria de python 
 #
-My_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('localhost', 12343)
-My_socket.connect(server_address)
+my_socket.connect(server_address)
 
 mensaje = sys.argv[1]
-print >>sys.stderr, 'envio "%s"'% mensaje
-My_socket.sendall(mensaje)
+print "Envio ", mensaje
+my_socket.sendall(mensaje)
 
 mensajeEsperado  = len (mensaje)
 mensajeRecivido = 0
 
 while mensajeRecivido < mensajeEsperado  :
-	datoServidor = My_socket.recv(16)
-	mensajeRecivido += len(datoServidor)
-	print >>sys.stderr, 'dato recivido del servidor "%s"' % datoServidor
-	 
-My_socket.close()
+    datoServidor = my_socket.recv(1024)
+    mensajeRecivido += len(datoServidor)
+    print "Dato recibido del servidor ", datoServidor
+    
+my_socket.close()
--- scripts/tcp/server_tcp.py
+++ scripts/tcp/server_tcp.py
@@ -1,25 +1,25 @@
 import socket
 import sys
 
-My_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('localhost', 12343)
-My_socket.bind(server_address)
-My_socket.listen(1)
+my_socket.bind(server_address)
+my_socket.listen(1)
 client_data = ''
 
 while True:
-	connection, client_address = My_socket.accept()
-	
-	try:
-		while client_data != 'fin':
-				client_data = connection.recv(15)
-				if client_data:
-					
-					connection.sendall(client_data.upper())
-					print >>sys.stderr, 'Enviando dato al cliente "%s"' % client_data.upper()
-				else:
-					print >>sys.stderr, 'no hay mas datos', client_address
-					break
-	finally:
-		connection.close()
+    connection, client_address = my_socket.accept()
+    
+    try:
+        while client_data != 'fin':
+            client_data = connection.recv(1024)
+	    if client_data:
+	        connection.sendall(client_data.upper())
+                print "Enviando dato al cliente: ", client_data.upper()
+	    else:
+                print "No hay mas datos"
+		break
+    finally:
+	connection.close()
+
 connection.close()
